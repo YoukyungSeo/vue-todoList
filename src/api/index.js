4332012d72662488ae7ea0bb12bb3c222def59c0
Aurora <s_yk@naver.com>
2022-05-16 16:57:06 +0900

    fix: 빌드

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 
 async function registerTodo(todoItems) {
-  const url = 'http://localhost:8080/todo';
+  const url = 'http://10.112.58.190:8080/todo';
   let res = await axios.post(url, todoItems);
   console.log(res);
 }
 
 async function deleteTodo(todoItems) {
-    const url = 'http://localhost:8080/todo?item='+todoItems.item;
+    const url = 'http://10.112.58.190:8080/todo?item='+todoItems.item;
     let res = await axios.delete(url);
     console.log(res);
 }
 
 async function toggleTodo(todoItems) {
-    const url = 'http://localhost:8080/todo';
+    const url = 'http://10.112.58.190:8080/todo';
     let res = await axios.put(url, todoItems);
     console.log(res);
 }
 
 async function clearAllTodo() {
-    const url = 'http://localhost:8080/todo';
+    const url = 'http://10.112.58.190:8080/todo';
     let res = await axios.get(url);
     console.log(res);
 }
 
 async function listTodo() {
-    const url = 'http://localhost:8080/todo/list';
+    const url = 'http://10.112.58.190:8080/todo/list';
     let res = await axios.get(url);
     return res.data;
 }
