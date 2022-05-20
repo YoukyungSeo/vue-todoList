8f443b6333c78b2a993844b16e700de96cee605c
Aurora <s_yk@naver.com>
2022-05-20 17:09:41 +0900

    Feat: 라우터 생성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
  <div id="app">
-  <todo-header></todo-header>
-  <todo-input></todo-input>
-  <todo-list></todo-list>
-  <todo-footer></todo-footer>
+  <router-view></router-view>
  </div>
 </template>
 
 <script>
-import TodoFooter from './component/TodoFooter.vue'
-import TodoHeader from './component/TodoHeader.vue'
-import TodoInput from './component/TodoInput.vue'
-import TodoList from './component/TodoList.vue'
-
-export default {
-  components: {
-    TodoHeader,
-    TodoInput,
-    TodoList,
-    TodoFooter
-  }
-}
 </script>
 
 <style>
