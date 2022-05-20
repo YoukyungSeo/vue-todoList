2f84637ca90890417d2ad0186f23dfa86e3ef216
Aurora <s_yk@naver.com>
2022-05-20 15:40:00 +0900

    Fix: URL 경로 변경

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,33 @@
 import axios from 'axios';
 
+const serverPath = 'http://10.112.58.190/todo'
+
 async function registerTodo(todoItems) {
-  const url = 'http://10.112.58.190:8080/todo';
+  const url = serverPath;
   let res = await axios.post(url, todoItems);
   console.log(res);
 }
 
 async function deleteTodo(todoItems) {
-    const url = 'http://10.112.58.190:8080/todo?item='+todoItems.item;
+    const url = serverPath+'?item='+todoItems.item;
     let res = await axios.delete(url);
     console.log(res);
 }
 
 async function toggleTodo(todoItems) {
-    const url = 'http://10.112.58.190:8080/todo';
+    const url = serverPath;
     let res = await axios.put(url, todoItems);
     console.log(res);
 }
 
 async function clearAllTodo() {
-    const url = 'http://10.112.58.190:8080/todo';
+    const url = serverPath;
     let res = await axios.get(url);
     console.log(res);
 }
 
 async function listTodo() {
-    const url = 'http://10.112.58.190:8080/todo/list';
+    const url = serverPath+'/list';
     let res = await axios.get(url);
     return res.data;
 }
